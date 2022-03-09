e5fd42b1117a8b0e4f66acd0873757332ee188a5
cmt13zzz <caochuongto@gmnail.com>
2022-03-09 15:08:09 +0700

    first commit

---
+++ react-ticket/src/redux/reducer/rootReducer.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+
+
+import BaiTapDatVeReducer from './BaiTapDatVeReducer';
+const rootReducer = combineReducers({ //store tổng của ứng dụng
+   
+    BaiTapDatVeReducer//state bài tập đặt vé
+})
+
+export default rootReducer;
